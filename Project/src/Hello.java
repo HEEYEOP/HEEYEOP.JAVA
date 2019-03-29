bb943d4886289540bf9fa7bf8b9a11c91daca8fa
gmlduq <gmlduqrhdwn@naver.com>
2019-03-29 14:10:33 +0900

    Day1

---
+++ Project/src/Hello.java
@@ -0,0 +1,149 @@
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.print("Hello");
+		System.out.print("Hello");*/
+		
+		/*int num = 0;
+		char ch ='A';
+		double num2 = 1.123;
+		boolean isEven = true;
+		
+		System.out.println(num);
+		System.out.println(ch);
+		System.out.println(num2);
+		System.out.println(isEven); */
+		
+		//MAX_SIZE 라는 상수에 리터럴 10의 값을 저장
+		/*final int MAX_SIZE = 10;
+		int num1 = 10;
+		long num2 =10L;
+		float num3 = 1.123f;
+		double num4 = 1.123d;*/
+		
+		/*Scanner scan = new Scanner(System.in);
+		int num = 0;
+		num = scan.nextInt();
+		System.out.println(num);
+		
+		char op = scan.next().charAt(0);
+		System.out.println(op);
+		*/
+		
+		/*산술연산자 /
+		 * 정수/정수 -> 명시적 형변환을 해줘야 원하는 값을 가질 수 있다.
+		 */
+		/*double num4 = 1/(double)2;
+		System.out.println(num4);
+		
+		System.out.println((char)('A'+1));
+		char ch ='A'+1;
+		System.out.println(ch);
+		char ch2 =(char)(ch+1);
+		System.out.println(ch2);
+		*/
+		
+		 /*int num =10;
+		 System.out.println("후위형 진행: " +num++);
+		 System.out.println("후위형 결과: " +num);
+		 
+		 num =10;
+		 System.out.println("전위형 결과: " + ++num);
+		 System.out.println("전위형 결과: " +num);*/
+		
+		/*int num2 =20;
+		
+		num2 += 1; //num2 =num2 +1; */
+		
+		
+		
+		
+		//Exam1
+		/*
+		int num5 =10, num6 = 3;
+		double res;
+		res = num5/(double)num6;
+		System.out.println(res);  */
+		
+	
+		//Exam2
+		/*Scanner scan= new Scanner(System.in);
+		int num2 = scan.nextInt();
+		System.out.println("입력한 수 : "+ num2);
+		scan.close();  */
+		
+		//Exam3
+		/*Scanner scan= new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.println(1 <= num && num <= 100);
+		scan.close();
+		*/
+		
+		//Exam3-1
+		/*Scanner scan= new Scanner(System.in);
+		int num = scan.nextInt();
+		boolean isScore = false;
+		isScore = 1<= num && num <= 100;
+		System.out.println(isScore);
+		scan.close();
+		*/
+		
+		//Exam4 (변수 선언하고 출력문으로 다 대체할 수 있음)
+		/*int num1 =10, num11=3;
+		int k;
+		k = num1+ num11;
+		System.out.println(k);
+		
+		
+		int num2= 10, num21 =3;
+		int t;
+		t = num2 -num21;
+		System.out.println(t);
+		
+		
+		int  h;
+		int num3 =10, num31 =3 ;
+		h = num3* num31;
+		System.out.println(h);
+
+		
+		
+		int num4 = 10, num41 =3;
+		double res;
+		res = num4 /(double)num41;
+		System.out.println(res);
+		
+		
+		
+		int num5;
+		num5 = 10%3;
+		System.out.println(num5);
+		*/
+	    
+		
+		//지금까지 먹은 밥값을 계산하여 결과출력
+		//하루에 3끼만 꼭 먹고 한끼에 3000원. 개월은 무시하고 나이로만 계산, 윤년은 무시
+		
+		/*int age = 25;
+		int day = 3;
+		int money = 3000;
+		int year =365;
+		int totalMoney = age* day* money* year;
+		
+		System.out.println("지금까지 먹은 금액:" + totalMoney + "원");*/
+		
+		
+		
+		
+		
+		
+		 
+			}
+
+}
+ 
