c752b0e81f2880e8d472b6d0c01ba2aa90f23ad7
gmlduq <gmlduqrhdwn@naver.com>
2019-04-03 17:10:28 +0900

    Day71

---
+++ Project/src/Day7/Day71.java
@@ -0,0 +1,32 @@
+package Day7;
+
+public class Day71 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// 1. 구구단 전체를 출력하는 코드를 작성하세요
+			int i, j ;
+			
+			for(i=2; i<=9; i++){
+				for(j=1; j<=9; j++){
+					System.out.println(i+"*"+j+ "="+i*j);
+				}
+				System.out.println(" ");
+			}
+			
+		
+			
+		
+			
+	
+		
+		
+		
+		
+		
+		
+	}
+
+}
