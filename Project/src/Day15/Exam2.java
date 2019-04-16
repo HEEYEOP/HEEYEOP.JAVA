30aa6174f849a35383dd0e4b8820ced1ad336048
gmlduq <gmlduqrhdwn@naver.com>
2019-04-16 17:30:16 +0900

    Exam2

---
+++ Project/src/Day15/Exam2.java
@@ -0,0 +1,21 @@
+package Day15;
+
+import java.util.Scanner;
+
+public class Exam2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		char a = scan.next().charAt(0);
+		String b = scan.next();
+		
+		System.out.println(num);
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
